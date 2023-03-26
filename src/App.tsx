226995458c33b20bfe083e1ff0cce51d3846a126
Rutger Walma <rutgerwalma@live.nl>
2023-03-26 17:05:20 +0200

    CSS centering fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 import { Hero } from "./components/Hero";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import styled from "styled-components";
 
 function App() {
   return (
     <HelmetProvider>
-      <div className="App">
+      <AppBox>
         <Helmet>
           <title>rutger.app</title>
         </Helmet>
-
         <Hero />
-      </div>
+      </AppBox>
     </HelmetProvider>
   );
 }
 
 export default App;
+
+const AppBox = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+`;
